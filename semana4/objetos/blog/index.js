d4257c64bc04e807b8041e5fd2245c1186bc2611
Diana Monteiro <dianar.monteiro@gmail.com>
2020-10-07 20:28:56 -0300

    Adicionando desafios

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,41 +1,38 @@
-let arrayPost = []
+let arrayPost = [];
+const tituloPost = document.getElementById("titulo-post");
+const autorPost = document.getElementById("autor-post");
+const conteudoPost = document.getElementById("conteudo-post");
+const containerPost = document.getElementById("container-de-posts");
+const imagemPost = document.getElementById("img-post");
 
-function criarPost () {
-const tituloPost= document.getElementById("titulo-post")
-const autorPost = document.getElementById("autor-post")
-const conteudoPost = document.getElementById("conteudo-post")
-const containerPost = document.getElementById("container-de-posts")
-const imagemPost = document.getElementById("img-post")
-
-let postBlog = {
+function inserirNoHTML() {
+  let postBlog = {
     titulo: tituloPost.value,
     autor: autorPost.value,
     conteudo: conteudoPost.value,
-    imagem: imagemPost.value
+    imagem: imagemPost.value,
+  };
+
+  arrayPost = [...arrayPost, postBlog];
+
+  if(postBlog.titulo !== "" && postBlog.autor !== "" && postBlog.conteudo !== "" && postBlog.imagem !== "") {
+    containerPost.innerHTML += `<h2>${postBlog.titulo}<h2><br>`;
+    containerPost.innerHTML += `<h3>Por:${postBlog.autor}<h3><br>`;
+    containerPost.innerHTML += `<p>${postBlog.conteudo}<p><br>`;
+    containerPost.innerHTML += `<img class="imgpost" src=${imagemPost.value}>`;
+  } else {
+    window.alert("Faça uma postagem maneira!");
+  }
 }
 
-arrayPost = [...arrayPost, postBlog]
-
-if (postBlog.titulo !== "" && postBlog.autor !== "" && postBlog.conteudo !== "" && postBlog.imagem !==""){
-    containerPost.innerHTML += `<h2>${postBlog.titulo}<h2>`
-    containerPost.innerHTML += `<h3>Por:${postBlog.autor}<h3>`
-    containerPost.innerHTML += `<p>${postBlog.conteudo}<p>`
-    containerPost.innerHTML += `<img class="imgpost" src=${imagemPost.value}>`
-}else {
-    window.alert("Faça uma postagem maneira!")
+function apagarInput() {
+  tituloPost.value = "";
+  autorPost.value = "";
+  conteudoPost.value = "";
+  imagemPost.value = "";
 }
 
-tituloPost.value = ""
-autorPost.value = ""
-conteudoPost.value = ""
-imagemPost.value = ""
-
+function criarPost() {
+  inserirNoHTML();
+  apagarInput();
 }
-
-// if(taskInput.value !== ""){ 
-//     day.innerHTML += `<li class="list" onclick="itsDone(this)"> &#10149; ${timeInput.value} - ${taskInput.value} </li>`
-// }else {
-//     alert("Digite uma nova tarefa!")
-// }
-// taskInput.value = ""
-
