6c6927944a326c20d9ebd3da2560ed0fe698cc44
Diana Monteiro <dianar.monteiro@gmail.com>
2020-10-09 11:47:31 -0300

    Adicionando exercício 1 de interpretação de código

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,7 +1,7 @@
 /*
 ------> Exercícios de interpretação de código <------
 
-1. # O que faz ==> O código converte um valor de um dinheiro em dólares para reais.
+1. # O que faz ==> O código converte o valor de um dinheiro em dólares para reais.
    # Como faz ==> A função conversorDeMoeda recebe um valor e pede o valor da cotação do dólar, retorna R$ e o valor inserido multiplicado pelo valor da cotação em dólares.
-   # Valor impresso ==> O valor impresso no console será de 100 multiplicado pela cotação do Dólar, que segundo o google esta em 5,54, então no console será impresso 554.
+   # Valor impresso ==> O valor impresso no console será de 100 multiplicado pela cotação do Dólar, que segundo o google esta em 5,54, então no console será impresso R$ 554.
 */
