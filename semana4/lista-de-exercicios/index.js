8f06e9655570599747bb8854066757c5571d1f55
Diana Monteiro <dianar.monteiro@gmail.com>
2020-10-10 00:53:28 -0300

    Adicionado exercício 3 de Funções de Array

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -280,9 +280,9 @@ console.log(menoresDeIdade)
 
 2.
 
-a)
-
 const arrayTeste = [1, 2, 3, 4, 5, 6]
+
+a)
 
 const vezesDois = arrayTeste.map((numero) => {
     return numero*2
@@ -312,5 +312,32 @@ const frases = arrayTeste.map((numero) => {
 })
 
 console.log(frases)
+
+3.
+
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+a)
+
+const permissaoParaEntrar = pessoas.filter((pessoas) => {
+    return (pessoas.altura >= 1.5) && (pessoas.idade > 14) && (pessoas.idade < 60)
+})
+
+console.log(permissaoParaEntrar)
+
+b
+
+const semPermissaoParaEntrar = pessoas.filter((pessoas) => {
+    return !permissaoParaEntrar.includes(pessoas)
+})
+
+console.log(semPermissaoParaEntrar)
 
 */
