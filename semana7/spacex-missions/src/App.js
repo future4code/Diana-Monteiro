83ffecc9223cb7d27d287a1cd1a5cdbdfb23f697
Diana Monteiro <dianar.monteiro@gmail.com>
2020-10-29 19:22:35 -0300

    Adicionando estilização

--- semana7/spacex-missions/src/App.js
+++ semana7/spacex-missions/src/App.js
@@ -1,16 +1,93 @@
 import React from "react";
+import "./App.css";
 import styled from "styled-components";
-import MissionsList from "./components/MissionsList"
+import MissionDetails from "./components/MissionDetails"
 import axios from "axios";
+import logo from "./logo.jpg";
+
+const Div = styled.div`
+  background-color: #375c7c;
+  height: 56vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+  font-family: 'Staatliches', cursive;
+`
+const Mission = styled.p`
+  text-align: center;
+  font-size: 23px;
+  &:hover {
+    color:#24252e;
+    cursor: pointer;
+  }
+`
+const P = styled.p`
+  color: #24252e;
+  margin: 20px 0;
+  font-size: 20px;
+`
+const H2 = styled.p`
+  font-size: 23px;
+  margin:0;
+  font-weight: 0;
+  margin-top: 5px;
+`
 
 export default class App extends React.Component {
+  state = {
+    allMissions: [],
+    missionDetails: [],
+    seeDetails: true
+  };
+
+  componentDidMount() {
+    this.getAllMissions();
+  }
+
+  getAllMissions = () => {
+    axios.get("https://api.spacexdata.com/v3/missions").then((response) => {
+      this.setState({ allMissions: response.data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  };
+
+  getOneMission = (mission) => {
+    this.seeAllDetails()
+
+    axios.get(`https://api.spacexdata.com/v3/missions/${mission}`).then((response) => {
+      this.setState({ missionDetails: response.data });
+      console.log(response)
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+  
+  seeAllDetails = () => {
+    this.setState({seeDetails: !this.state.seeDetails})
+  }
+
   render() {
+    const list = this.state.allMissions.map((mission) => {
+      return <Mission key={mission.mission_id} onClick={() => this.getOneMission (mission.mission_id)}>{mission.mission_name}</Mission>;
+    });
+
     return (
+      <Div>
+      <img src={logo}/>
+      <H2>MISSIONS</H2>
+      {this.state.seeDetails ?
       <div>
-        <h1>SpaceX</h1>
-        <h3>MISSIONS</h3>
-        <MissionsList/>
+          <P>Clique em uma missão e veja mais sobre ela!</P>
+          <div>{list}</div>
       </div>
+      :
+      <MissionDetails goBack={this.seeAllDetails} missionDetails={this.state.missionDetails} />
+      }
+      </Div>
     );
   }
 }
--- semana7/spacex-missions/src/components/MissionDetails.js
+++ semana7/spacex-missions/src/components/MissionDetails.js
@@ -1,19 +1,36 @@
 import React from "react";
 import styled from "styled-components";
-import axios from "axios";
+
+const Div = styled.div`
+  border: 1px solid #24252e;
+  width: 50vw;
+  margin: 30px 0;
+`
+const P = styled.p`
+  margin: 30px;
+`
+const Button = styled.button`
+  background-color: #64849c;
+  margin-bottom: 25px;
+  margin-left: 34em;
+  color: #24252e;
+  width: 200px;
+  height: 30px;
+  font-family: 'Staatliches', cursive;
+`
 
 class MissionDetails extends React.Component {
   render() {
     console.log(this.props.missionDetais)
     return (
-      <div>
-      <p>Nome da Missão:{this.props.missionDetails.mission_name}</p>
-      <p>Descrição:{this.props.missionDetails.description}</p>
-      <p>Fabricantes:{this.props.missionDetails.manufactures}</p>
-      <p>Wikipédia:<a href={this.props.missionDetails.wikipedia} target="_blank">{this.props.missionDetails.wikipedia}</a></p>
-      <p>Website:<a href={this.props.missionDetails.website} target="_blank">{this.props.missionDetails.website}</a></p>
-      <button onClick={this.props.goBack}>Voltar</button>
-      </div>
+      <Div>
+      <P>Nome da Missão: {this.props.missionDetails.mission_name}</P>
+      <P>Descrição: {this.props.missionDetails.description}</P>
+      <P>Fabricantes: {this.props.missionDetails.manufacturers}</P>
+      <P>Wikipédia: <a href={this.props.missionDetails.wikipedia} target="_blank">{this.props.missionDetails.wikipedia}</a></P>
+      <P>Website: <a href={this.props.missionDetails.website} target="_blank">{this.props.missionDetails.website}</a></P>
+      <Button onClick={this.props.goBack}>Voltar</Button>
+      </Div>
     );
   }
 }
