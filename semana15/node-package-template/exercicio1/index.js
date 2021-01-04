dc2ae8cf908db638a95961e56cbb77f15cd0f2cd
Diana Monteiro <dianar.monteiro@gmail.com>
2021-01-04 17:03:54 -0300

    Adicionando exercício aula 40

---
+++ semana15/node-package-template/exercicio1/index.js
@@ -0,0 +1,17 @@
+//Exercício 1
+
+// a)
+// Acessamos os parâmetros passados na linha de comando através da propriedade process.argv.
+
+//b)
+
+const name = "Diana"
+const age = 32
+
+// console.log(`Olá, ${name}! Você tem ${age} anos.`)
+
+//c)
+
+const agePlusSeven = Number(age + 7)
+
+console.log(`Olá, ${name}! Você tem ${age} anos. Em sete anos você terá ${agePlusSeven}.`)
---
+++ semana15/node-package-template/exercicio3/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const task = process.argv[2]
+const taskList = []
+const addTasks = taskList.push(task)
+
+
+console.log(taskList)
