d5292c7fa01ff40fc9504025307f731a19c40e35
Diana Monteiro <dianar.monteiro@gmail.com>
2021-01-26 23:59:38 -0300

    Adicionando exercicio-aula51

---
+++ semana18/boilerplate_autenticacao/src/data/selectUserById.ts
@@ -0,0 +1,9 @@
+import { connection } from "./config";
+
+export default async function selectUserById(id: string): Promise<any> {
+    const result = await connection('User_aula50')
+        .select('*')
+        .where({ id })
+
+    return result[0]
+}
