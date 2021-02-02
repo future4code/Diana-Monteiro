c5f1cbb256da70b39dbf78edaca4de4dacdb2717
Diana Monteiro <dianar.monteiro@gmail.com>
2021-02-01 21:17:19 -0300

    Adiciando exercicio

--- semana19/backend-template/src/business/services/authenticator.ts
+++ semana19/backend-template/src/business/services/authenticator.ts
@@ -1,5 +1,6 @@
 // Libraries
 import * as jwt from 'jsonwebtoken'
+import { authenticationData } from "../entities/user"
 
 export const generateToken = (id: string): string => {
     const token: string = jwt.sign(
@@ -15,8 +16,4 @@ export const getTokenData = (token:string): authenticationData => {
     const payload = jwt.verify(token, process.env.JWT_KEY!);
 
     return payload as authenticationData
-}
-
-export type authenticationData = {
-    id: string
 }
